51a70f78f73c2c1901644ba976bb5a8022e77256
LucasFurioFranco <lucasfuriofranco@gmail.com>
2023-01-06 21:01:34 -0300

    17. Componente com Propriedade
    1 - Passagem de parâmmetros para o omponente por meio de atributos "HTML" na chamada/instância de sua respectiva tag HTML
    
    2 - Para utilizar o valor no componente, a função que o define espera um parâmetro (pode ter qualquer nome, mas idealmente chamamos de "props") que será um objeto, onde suas chaves e valores são definidos pelos atributos passados em sua chamada (citado em #1)

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
+import CommParametro from './components/basicos/ComParametro'
 
 ReactDOM.render(
     <div>
         <Primeiro></Primeiro>
+        <CommParametro
+            titulo="Situação do Aluno"
+            aluno="Pedro Silva"
+            nota={9.3}
+        />
+        <CommParametro
+            titulo="Situação do Aluno"
+            aluno="Maria"
+            nota={9.9}
+        />
     </div>,
     document.getElementById("root")
 )
